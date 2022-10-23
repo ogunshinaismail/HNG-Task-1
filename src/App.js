33e3736201b19722fbeca986128045cd9a2d1904
ismail <ismailogunshina@gmail.com>
2022-10-23 20:20:11 +0800

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Profile } from '../src/assets'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <div id="profile">
+        <img src={Profile} alt="dp" id='profile__img' />
+        <h3 id='twitter'>Ogunshina Ismail</h3>
+        <p id='slack'>Ismail</p>
+      </div>
+
+      <div id="links">
+        <a href="#">Twitter Link</a>
+        <a href="#" id='btn__zuri'>Zuri Team</a>
+        <a href="#" id='books'>Zuri Books</a>
+        <a href="#" id='book__python'>Python Books</a>
+        <a href="#" id='pitch'>Background Check for Coders</a>
+        <a href="#" id='book__design'>Design Books</a>
+      </div>
     </div>
   );
 }
